1b8aba1dd6ff4c532a3578be83d4620829712a25
ymj0828 <ymj369@nate.com>
2023-07-24 23:31:15 +0900

    add: stop if no menual value

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
   const API_KEY = `f40fc02dbebc40a3a6c4`
+
+  const menual_reg = /MANUAL[1-20]/
   
   //////////////// 음식명 검색 시 실행될 코드 /////////////////////
   const cook_form = document.querySelector('.cook_form');
@@ -22,17 +24,34 @@ window.addEventListener('DOMContentLoaded', () => {
       let data = await response.json();
       let data2 = data.COOKRCP01.row
       console.log(data2)
-
+      
       // forEach를 사용하여 여러개의 데이터를 하나씩 선택할 수 있다.
       // data2의 개수만큼 반복한다.
       data2.forEach((recipe_data)=>{
-        
+        let menual_arr = []
+        for(key in recipe_data){
+          if(recipe_data[key]!=''){
+            if(menual_reg.test(key)){
+              menual_arr.push(recipe_data[key])
+            }
+          }
+        }
+
+        menual_arr = menual_arr.sort()
+
+        for(let i = 0; i < menual_arr.length; i++){
+          if(menual_arr[i].charAt(0) != i+1){
+            return
+          }
+        }
+
+
         // 해물김치찌개 사진이 이상하여 if문을 사용해 검색결과로 해물김치찌개가 나오면 코드 실행이 되지 않게 하였다.
         if(recipe_data.RCP_NM == '해물김치찌개'){
           return
         }
         
-
+        
         // 요소 만들기
         const recipe_list_li = document.createElement('li');
         const rcp_img_wrap = document.createElement('div');
@@ -41,8 +60,8 @@ window.addEventListener('DOMContentLoaded', () => {
         
         // 요소를 부모 요소 맨 뒤에 추가하기(연결)
         recipe_list_wrap.append(recipe_list_li);
-        recipe_list_li.append(rcp_img_wrap, rcp_nm)
-        rcp_img_wrap.append(rcp_img)
+        recipe_list_li.append(rcp_img_wrap, rcp_nm);
+        rcp_img_wrap.append(rcp_img);
         
         // 요소의 속성을 만들고(data-내가 정한 이름) 레시피 이름을 속성값으로 넣어주었다.
         // 검색 결과로 나온 음식을 클릭할 때 이 레시피 이름(속성값)을 받아와 sub페이지에 해당 레시피 데이터를 나오게 할 수 있다.
@@ -54,8 +73,12 @@ window.addEventListener('DOMContentLoaded', () => {
         
         // 데이터에서 음식사진 경로를 받아와 rcp_img의 src에 넣어주어 사진이 나오도록 하였다.
         rcp_img.src = recipe_data.ATT_FILE_NO_MAIN
-
+        if(recipe_data.ATT_FILE_NO_MAIN == ''){
+            rcp_img.src = recipe_data.ATT_FILE_NO_MK
+        }
+        
         rcp_nm.innerText = recipe_data.RCP_NM
+
         
       })
       
@@ -122,6 +145,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
         rcp_img.src = recipe_data.ATT_FILE_NO_MAIN
+
+
+
         rcp_nm.innerText = recipe_data.RCP_NM
 
       })
