9fdcef0a4b9b2af648c30e1358effb3a479d0977
YangHK95 <ekzm7040@naver.com>
2023-07-28 19:15:17 +0900

    grid

--- js/main.js
+++ js/main.js
@@ -1,6 +1,83 @@
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("DOMContentLoaded", async () => {
   const API_KEY = `f40fc02dbebc40a3a6c4`;
+  // const API_KEY2 = `125a942cee624a03b4a3`;
 
+  const now = new Date();
+  const day = now.getDate();
+  console.log(day);
+
+  let url = `http://openapi.foodsafetykorea.go.kr/api/${API_KEY}/COOKRCP01/json/1/40`;
+  let response = await fetch(url);
+  let data = await response.json();
+  let data2 = data.COOKRCP01.row;
+
+  if (data2.RCP_NM == "해물김치찌개") {
+    return;
+  }
+  //console.log("data2", data2[0]);
+
+  // 오늘의 추천 메뉴
+  const recipe_recommand = document.querySelector(".recipe_recommand");
+  const recipe_recommand_li = document.createElement("li");
+  const recipe_recommand_li2 = document.createElement("li");
+  const recipe_recommand_li3 = document.createElement("li");
+  const recipe_recommand_li4 = document.createElement("li");
+  const recipe_recommand_li5 = document.createElement("li");
+  const recipe_recommand_li6 = document.createElement("li");
+  const recipe_recommand_li7 = document.createElement("li");
+  const rcp_img2_recommand = document.createElement("div");
+
+  const rcp_img2 = document.createElement("img");
+  const rcp_nm2 = document.createElement("p");
+  const rcp_img3 = document.createElement("img");
+  const rcp_nm3 = document.createElement("p");
+  const rcp_img4 = document.createElement("img");
+  const rcp_nm4 = document.createElement("p");
+  const rcp_img5 = document.createElement("img");
+  const rcp_nm5 = document.createElement("p");
+  const rcp_img6 = document.createElement("img");
+  const rcp_nm6 = document.createElement("p");
+  const rcp_img7 = document.createElement("img");
+  const rcp_nm7 = document.createElement("p");
+
+  // 오늘의 추천 메뉴 보여줘!!
+  recipe_recommand.append(recipe_recommand_li);
+  recipe_recommand.append(recipe_recommand_li2);
+  recipe_recommand.append(recipe_recommand_li3);
+  recipe_recommand.append(recipe_recommand_li4);
+  recipe_recommand.append(recipe_recommand_li5);
+  recipe_recommand.append(recipe_recommand_li6);
+  recipe_recommand.append(recipe_recommand_li7);
+  recipe_recommand_li.append(rcp_img2, rcp_nm2);
+  recipe_recommand_li2.append(rcp_img3, rcp_nm3);
+  recipe_recommand_li3.append(rcp_img4, rcp_nm4);
+  recipe_recommand_li4.append(rcp_img5, rcp_nm5);
+  recipe_recommand_li5.append(rcp_img6, rcp_nm6);
+  recipe_recommand_li6.append(rcp_img7, rcp_nm7);
+
+  // 속성
+  recipe_recommand_li.setAttribute("data-RCP_NM", data2.RCP_NM);
+  rcp_img2_recommand.classList.add("rcp_img_recommand");
+  recipe_recommand_li.classList.add("rcp_recommand_img");
+  recipe_recommand_li2.classList.add("rcp_recommand_img2");
+  recipe_recommand_li3.classList.add("rcp_recommand_img3");
+  recipe_recommand_li4.classList.add("rcp_recommand_img4");
+  recipe_recommand_li5.classList.add("rcp_recommand_img5");
+  recipe_recommand_li6.classList.add("rcp_recommand_img6");
+
+  rcp_nm2.classList.add("rcp_nm2");
+  rcp_img2.src = data2[day].ATT_FILE_NO_MAIN;
+  rcp_nm2.innerText = data2[day].RCP_NM;
+  rcp_img3.src = data2[day + 1].ATT_FILE_NO_MAIN;
+  rcp_nm3.innerText = data2[day + 1].RCP_NM;
+  rcp_img4.src = data2[day + 2].ATT_FILE_NO_MAIN;
+  rcp_nm4.innerText = data2[day + 2].RCP_NM;
+  rcp_img5.src = data2[day + 3].ATT_FILE_NO_MAIN;
+  rcp_nm5.innerText = data2[day + 3].RCP_NM;
+  rcp_img6.src = data2[day + 4].ATT_FILE_NO_MAIN;
+  rcp_nm6.innerText = data2[day + 4].RCP_NM;
+  rcp_img7.src = data2[day + 5].ATT_FILE_NO_MAIN;
+  rcp_nm7.innerText = data2[day + 5].RCP_NM;
   const cook_form = document.querySelector(".cook_form");
   const cook_search = document.querySelector(".cook_search");
   const ingre_form = document.querySelector(".ingre_form");
