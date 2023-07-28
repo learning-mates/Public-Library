4199fdd5fae471c51d44e4ce1a8bdd91c7ca8710
ymj0828 <ymj369@nate.com>
2023-07-28 18:04:22 +0900

    원상복구

--- js/sub.js
+++ js/sub.js
@@ -2,18 +2,15 @@ window.addEventListener('DOMContentLoaded', () => {
   const API_KEY = `f40fc02dbebc40a3a6c4`
   let recipe_name = decodeURI(location.href)
   recipe_name = recipe_name.split('?')[1];
-
   // 정규식 선언
   const menual_reg = /MANUAL[1-20]/  // MANUAL이 있고 그 뒤에 1~20까지 숫자가 있으면
   const menual_img_reg = /MANUAL_IMG/  // MANUAL_IMG가 있으면
-
   // 배열 선언 (빈 배열)
   let menual_arr = []
   let menual_img_arr = []
 
 
   async function getData(){
-
     let url = `http://openapi.foodsafetykorea.go.kr/api/${API_KEY}/COOKRCP01/json/1/1000/RCP_NM=${recipe_name}/`
     const response = await fetch(url)
     const data = await response.json();
@@ -21,6 +18,7 @@ window.addEventListener('DOMContentLoaded', () => {
     // console.log(recipe_data)
 
     
+
     // const container = document.querySelector('.container');
     // const menu_img_wrap = document.querySelector('.menu_img_wrap');
     const menu_img = document.querySelector('.menu_img_wrap > img');
@@ -59,6 +57,7 @@ window.addEventListener('DOMContentLoaded', () => {
     info_fat.innerHTML = `<strong>지방</strong><span>${recipe_data.INFO_NA} g</span>`
     rcp_na_tip.innerText = recipe_data.RCP_NA_TIP
 
+
     if(recipe_data.RCP_NA_TIP == ''){
       rcp_na_tip_wrap.remove()
     }
@@ -69,34 +68,24 @@ window.addEventListener('DOMContentLoaded', () => {
     // rcp_dtls.classList.add('rcp_dtls');
     // menual_list.classList.add('menual_list');
 
-    // for문을 사용하여 recipe_data의 key 개수만큼 코드를 반복하게 하였다.
+
+
+    
     for(key in recipe_data){
-
-      // 만약 데이터가 빈 값이 아니라면
       if(recipe_data[key]!=''){
-
-        // 만약 key가 정규식 menual_reg 조건에 맞는다면
         if(menual_reg.test(key)){
-
-          // 배열 menual_arr 뒤에 메뉴얼(글)을 추가한다.
           menual_arr.push(recipe_data[key])
         }
-
-        // 만약 key가 정규식 menual_img_reg 조건에 맞는다면
         if(menual_img_reg.test(key)){
-
-          // 배열 menual_img_arr 뒤에 메뉴얼(이미지)을 추가한다.
           menual_img_arr.push(recipe_data[key])
         }
       }
     }
-
-    // 배열을 정렬한다.
     menual_arr = menual_arr.sort()
     menual_img_arr = menual_img_arr.sort()
     
-    // forEach문을 사용하여 메뉴얼 배열 안 요소의 개수만큼 반복한다.
-    menual_arr.forEach((menual_arr_ele, menual_arr_index)=>{
+
+    menual_arr.forEach((ele, index)=>{
       const menual_li = document.createElement('li')
       const menual_li_txt = document.createElement('p')
       const menual_li_img_wrap = document.createElement('div')
@@ -104,13 +93,18 @@ window.addEventListener('DOMContentLoaded', () => {
 
       menual_li_img_wrap.classList.add('menual_li_img_wrap')
 
-      menual_li_img.src = menual_img_arr[menual_arr_index]
-      menual_li_txt.innerText = menual_arr_ele
+      menual_li_img.src = menual_img_arr[index]
+      menual_li_txt.innerText = ele
 
       menual_list.append(menual_li)
       menual_li.append(menual_li_img_wrap, menual_li_txt)
       menual_li_img_wrap.append(menual_li_img)
     })
+
+
+
+
+
 
 
 
